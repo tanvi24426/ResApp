adef07f4d92687402613a1b71e5852c32eac1f42
Akshat Vashisht <akashat2003@gmail.com>
2024-01-31 00:07:22 +0530

    Update Resolution.kt

--- app/src/main/java/com/example/new_year_resolutions/Resolution.kt
+++ app/src/main/java/com/example/new_year_resolutions/Resolution.kt
@@ -1,5 +1,9 @@
 package com.example.new_year_resolutions
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.MutableState
+
 data class Resolution (
-    var name:String
+    var name:String,
+    var editedName: MutableState<String> = mutableStateOf(name)
 )
